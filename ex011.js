90a974c516bdd954d29f678d4a26d92cd9803aca
Emanuel Victor Fontoura Cantalejo <103543476+EmanuelVFC@users.noreply.github.com>
2022-08-01 23:40:42 -0300

    aulas

---
+++ ex011.js
@@ -0,0 +1,11 @@
+var idade = 15
+
+if (idade < 16){
+    console.log('Não vota')
+}else{
+    if (idade < 18){
+        console.log('Voto facultativo!')
+    }else{
+        console.log('Voto obrigatório!')
+    }
+}
