4f8a123a9c3fa8eba37a0d3dfba66991e2830459
Emanuel Victor Fontoura Cantalejo <103543476+EmanuelVFC@users.noreply.github.com>
2022-08-12 09:58:34 -0300

    update

--- praticar/ex015/script.js
+++ praticar/ex015/script.js
@@ -16,30 +16,53 @@ function verificar(){
 
         var resposta = window.document.getElementById('itexto')
 
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
         if (sexo[0].checked){
             genero = "Homem"
+
+            if (idade > 0 && idade <= 5){
+                fase_vida = "Bebê"
+                img.setAttribute('src', 'bebe-homem.jpg')
+            }else if (idade <= 12){
+                fase_vida = "Criança"
+                img.setAttribute('src', 'crianca-homem.jpg')
+            }else if (idade <= 16){
+                fase_vida = "Adolescente"
+                img.setAttribute('src', 'adolescente-homem.jpg')
+            }else if (idade <= 25){
+                fase_vida = "Jovem"
+                img.setAttribute('src', 'jovem-homem.jpg')
+            }else if (idade <= 60){
+                fase_vida = "Adulto"
+                img.setAttribute('src', 'adulto-homem.jpg')
+            }else if (idade <= 130){
+                fase_vida = "Idoso"
+                img.setAttribute('src', 'idoso-homem.jpg')
+            }
         }else if (sexo[1].checked){
             genero = "Mulher"
-        }
 
-        if (idade > 0 && idade <= 5){
-            fase_vida = "Bebê"
-            
-        }else if (idade <= 12){
-            fase_vida = "Criança"
-            
-        }else if (idade <= 16){
-            fase_vida = "Adolescente"
-            
-        }else if (idade <= 25){
-            fase_vida = "Jovem"
-            
-        }else if (idade <= 60){
-            fase_vida = "Adulto"
-
-        }else if (idade <= 130){
-            fase_vida = "Idoso"
-
+            if (idade > 0 && idade <= 5){
+                fase_vida = "Bebê"
+                img.setAttribute('src', 'bebe-mulher.jpg')
+            }else if (idade <= 12){
+                fase_vida = "Criança"
+                img.setAttribute('src', 'crianca-mulher.jpg')
+            }else if (idade <= 16){
+                fase_vida = "Adolescente"
+                img.setAttribute('src', 'adolescente-mulher.jpg')
+            }else if (idade <= 25){
+                fase_vida = "Jovem"
+                img.setAttribute('src', 'jovem-mulher.jpg')
+            }else if (idade <= 60){
+                fase_vida = "Adulto"
+                img.setAttribute('src', 'adulto-mulher.jpg')
+            }else if (idade <= 130){
+                fase_vida = "Idoso"
+                img.setAttribute('src', 'idoso-mulher.jpg')
+            }
         }
     }
 }
