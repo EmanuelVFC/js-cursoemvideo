3796aa7ca53bcbd7b8b0e9cfe4e4e8ecc8df5f2f
Emanuel Victor Fontoura Cantalejo <103543476+EmanuelVFC@users.noreply.github.com>
2022-09-01 23:51:49 -0300

    Update script.js

--- praticar/ex022/script.js
+++ praticar/ex022/script.js
@@ -4,38 +4,12 @@ let dados = []
 let local = window.document.getElementById('ilista')
 
 function adicionar(){
-    if (isNumero(num) && !inLista(num, dados)){
+    if (num >= 1 && num <= 100 && dados.indexOf(num) != -1){
         dados.push(num)
         let item = document.createElement('option')
         item.text = `O valor ${num} foi adicionado.`
         local.appendChild(item)
     }else{
-        window.alert('Valor inválido ou já encontrado na lista.')
+        window.alert("Valor inválido ou já encontrado na lista.")
     }
 }
-
-function finalizar(){
-    let final = window.document.getElementById('ifinal')
-    let item = document.createElement('div')
-    let tamanho = dados.length
-
-    item.text = `Ao todo, temos ${tamanho} números cadastrados.`
-    final.appendChild(item)
-}
-
-function isNumero(n){
-    if (n >= 1 && n <= 100){
-        return true
-    }else{
-        return false
-    }
-}
-
-function inLista(n, l){
-    if (l.indexOf(n) == -1){
-        return false
-    }else{
-        return true
-    }
-}
-
